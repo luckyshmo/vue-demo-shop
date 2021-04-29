be006fe37471e6a16522e7be3144b3205f507c5a
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-04-29 17:37:52 +0500

    Add all scripts

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import axios from "axios";
+import commonActions from './actions/actions'
+import apiRequests from './actions/api-requests'
+import mutations from "./mutations/mutations"
+import getters from "./getters/getters";
+
+const actions = {...commonActions, ...apiRequests}
 
 Vue.use(Vuex);
 
@@ -13,31 +18,9 @@ let store = new Vuex.Store({
     products: [],
     cart: []
   },
-  mutations: { //sync
-    SET_PRODUCTS_TO_STATE: (state, products) => {
-        state.products = products;
-      },
-  },
-  actions: { //async
-    GET_PRODUCTS_FROM_API({commit}) {
-        return axios('http://localhost:3000/products', {
-          method: "GET"
-        })
-          .then((products) => {
-            commit('SET_PRODUCTS_TO_STATE', products.data);
-            return products; //для использования в компоненте
-          })
-          .catch((error) => {
-            console.log(error)
-            return error; //для использования в компоненте
-          })
-      },
-  },
-  getters: {
-    PRODUCTS(state) {
-        return state.products;
-    },
-  },
+  mutations,
+  actions,
+  getters,
 });
 
 export default store;
